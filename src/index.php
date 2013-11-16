78e53d54df41af8dfe2464b9e1b565c8b8c82b0a
brendancahill8 <brendan.cahill8@gmail.com>
2013-11-16 20:11:26 +1100

    Added some test scss

--- src/index.php
+++ src/index.php
@@ -3,8 +3,19 @@
 <head>
 	<meta charset="UTF-8">
 	<title>New Document</title>
+	<link rel="stylesheet" href="/assets/css/main.css">
 </head>
 <body>
+
 	<h1><?php echo "Hello world!"; ?></h1>
+
+	<p>Before</p>
+	
+	<div id="test">
+		<p>During</p>
+	</div>
+
+	<p>After</p>
+
 </body>
 </html>
